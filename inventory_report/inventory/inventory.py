f9547db721697868d59425866f6db766604e6cec
Veronica-Alfr <veagalves@gmail.com>
2022-11-03 17:12:00 -0300

    feat: criado função para leitura de arquivo xml

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,6 +1,6 @@
 import csv
 import json
-# import xmltodict
+import xmltodict
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -23,3 +23,6 @@ class Inventory:
         elif path.endswith('json'):
             with open(path) as json_file:
                 return json.loads(json_file.read())
+        elif path.endswith('xml'):
+              with open(path) as xml_file:
+                return xmltodict.parse(xml_file.read())["dataset"]["record"]
