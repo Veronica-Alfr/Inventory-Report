fae448dceab053d46bfaaffb4a879fdbb1666251
Veronica-Alfr <veagalves@gmail.com>
2022-11-03 16:54:50 -0300

    feat: criado class Inventory que lê arquivos json e csv

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,25 @@
+import csv
+import json
+# import xmltodict
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+
+class Inventory:
+
+    @classmethod
+    def import_data(cls, path, report):
+        data = cls.change_path(path)
+        if report == "simples":
+            return SimpleReport.generate(data)
+        else:
+            return CompleteReport.generate(data)
+
+    @classmethod
+    def change_path(cls, path):
+        if path.endswith('csv'):
+            with open(path) as csv_file:
+                return list(csv.DictReader(csv_file))
+        elif path.endswith('json'):
+            with open(path) as json_file:
+                return json.loads(json_file.read())
